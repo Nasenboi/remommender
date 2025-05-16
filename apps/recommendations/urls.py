b9670acb0f4299732cf806f150d22b4a3e733d74
Simon Ludwig <simon.ludwig@study.hs-duesseldorf.de>
2025-05-17 00:24:42 +0200

    added speech emotion recognition API endpoint (closes #1)

---
+++ apps/recommendations/urls.py
@@ -0,0 +1,8 @@
+from django.urls import re_path
+
+from apps.recommendations.views import RecommendFromSpeechView
+
+app_name = 'recommendations'
+urlpatterns = [
+    re_path(r'from-speech/(?P<filename>[^/]+)$', RecommendFromSpeechView.as_view())
+]
