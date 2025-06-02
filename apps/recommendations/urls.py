b9071eb85676ac69c458e7d4ee8dd646ec32fe4d
Christian Böndgen <c.boendgen@gmail.com>
2025-06-02 10:04:40 +0200

    add another recommendation endpoint

--- apps/recommendations/urls.py
+++ apps/recommendations/urls.py
@@ -1,8 +1,12 @@
 from django.urls import re_path
 
-from apps.recommendations.views import RecommendFromSpeechView
+from apps.recommendations.views import (
+    RecommendFromSpeechView,
+    RecommendFromEmotionFeatures,
+)
 
 app_name = "recommendations"
 urlpatterns = [
-    re_path(r"from-speech/(?P<filename>[^/]+)$", RecommendFromSpeechView.as_view())
+    re_path(r"from-speech/(?P<filename>[^/]+)$", RecommendFromSpeechView.as_view()),
+    re_path(r"from-emotion-features$", RecommendFromEmotionFeatures.as_view()),
 ]
--- apps/recommendations/views.py
+++ apps/recommendations/views.py
@@ -6,13 +6,52 @@ from rest_framework.views import APIView
 
 from packages.emotion_recognition.processor import SERProcessor
 
+from packages.hsd_recommender.models import EmotionFeatures
+from packages.hsd_recommender.hsd_recommender import HSDRecommender
+
 
 class RecommendFromSpeechView(APIView):
     parser_classes = [FileUploadParser]
     speech_processor = SERProcessor()
+    hsd_recommender = HSDRecommender()
 
     def post(self, request, filename, format=None):
         file = request.data["file"]
-        speech_emotion_result = self.speech_processor.process_audio_file(file)
+        speech_emotion_result = dataclasses.asdict(
+            self.speech_processor.process_audio_file(file)
+        )
+        valence = float(speech_emotion_result["valence"])
+        arousal = float(speech_emotion_result["arousal"])
+        dominance = float(speech_emotion_result["dominance"])
+        authenticity = valence * dominance
+        timeliness = arousal * dominance
+        complexity = dominance
 
-        return Response(status=200, data=dataclasses.asdict(speech_emotion_result))
+        emotion_features = EmotionFeatures(
+            valence=valence,
+            arousal=arousal,
+            dominance=dominance,
+            authenticity=authenticity,
+            timeliness=timeliness,
+            complexity=complexity,
+        )
+
+        playlist = self.hsd_recommender.generate_emotional_playlist(
+            emotionFeatures=emotion_features
+        )
+
+        return Response(status=200, data=playlist)
+
+
+class RecommendFromEmotionFeatures(APIView):
+    hsd_recommender = HSDRecommender()
+
+    def post(self, request, format=None):
+        emotion_features_data = request.data
+        emotion_features = EmotionFeatures(**emotion_features_data)
+
+        playlist = self.hsd_recommender.generate_emotional_playlist(
+            emotionFeatures=emotion_features
+        )
+
+        return Response(status=200, data=playlist)
