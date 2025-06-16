71b929d340be06cd001f30e265e5994211e2f328
Christian Böndgen <c.boendgen@gmail.com>
2025-06-16 10:13:28 +0200

    add clear session endpoint

--- apps/session/api.py
+++ apps/session/api.py
@@ -3,14 +3,25 @@ from ninja.responses import Response
 
 router = Router()
 
+
 @router.post("/start")
-def start_session(request):
+def start_session(request) -> Response:
     session = request.session
     session["songs_played"] = []
     session.save()
     return {"session_key": session.session_key, "message": "Session started"}
 
+
 @router.post("/end")
-def end_session(request):
-    request.session.flush()
+def end_session(request) -> Response:
+    session = request.session
+    session.flush()
     return {"message": "Session ended"}
+
+
+@router.post("/clear")
+def clear_session(request) -> Response:
+    session = request.session
+    session["songs_played"] = []
+    session.save()
+    return {"message": "Session cleared"}
