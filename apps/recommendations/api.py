ed38db8e7dc8e3aa29fcfc35e750061f75917d8d
Christian Böndgen <c.boendgen@gmail.com>
2025-06-29 11:59:18 +0200

    add query args

--- apps/recommendations/api.py
+++ apps/recommendations/api.py
@@ -1,11 +1,14 @@
 import dataclasses
 
+from typing import Optional
+
 from ninja import Router
 from ninja.files import UploadedFile
 
 from packages.emotion_recognition.processor import SERProcessor
 from packages.hsd_recommender.hsd_recommender import HSDRecommender
 from packages.hsd_recommender.models import SongFeatures
+from packages.hsd_recommender.consts import GENRE_DATA_BASE
 from packages.emotion_recognition.models import EmotionFeatures
 
 from .schemas import (
@@ -38,13 +41,29 @@ def get_emotion_features_from_speech(file: UploadedFile) -> EmotionFeatures:
 
 @router.post("/from-speech", response=RecommendFromSpeechResponseSchema)
 def recommend_from_speech(
-    request, file: UploadedFile  # todo add genre query
+    request,
+    file: UploadedFile,
+    genre: Optional[GENRE_DATA_BASE] = "none",
+    authenticity: Optional[float] = None,
+    timeliness: Optional[float] = None,
+    complexity: Optional[float] = None,
+    danceable: Optional[float] = None,
+    tonal: Optional[float] = None,
+    voice: Optional[float] = None,
+    bpm: Optional[int] = None,
 ) -> RecommendFromSpeechResponseSchema:
     """
     Recommend a playlist based on speech emotion recognition.
     :param request: The request object
     :param file: Uploaded audio file
-    :param query: Query parameters for the recommendation
+    :param genre: Genre filter for the recommendation
+    :param authenticity: Authenticity feature for the recommendation
+    :param timeliness: Timeliness feature for the recommendation
+    :param complexity: Complexity feature for the recommendation
+    :param danceable: Danceable feature for the recommendation
+    :param tonal: Tonal feature for the recommendation
+    :param voice: Voice feature for the recommendation
+    :param bpm: Beats per minute feature for the recommendation
     :return: A playlist based on the emotion features extracted from the speech
     """
 
@@ -56,11 +75,18 @@ def recommend_from_speech(
     features = SongFeatures(
         valence=emotion_features.valence,
         arousal=emotion_features.arousal,
+        authenticity=authenticity,
+        timeliness=timeliness,
+        complexity=complexity,
+        danceable=danceable,
+        tonal=tonal,
+        voice=voice,
+        bpm=bpm,
     )
 
     # todo combine emotion features from speech and text
 
-    playlist = hsd_recommender.generate_playlist(songFeatures=features)
+    playlist = hsd_recommender.generate_playlist(songFeatures=features, genre=genre)
 
     # todo use a song thats not in played_songs
     response = RecommendFromSpeechResponseSchema(
