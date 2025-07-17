cdcf3032ad2c4ae6de5f5fa99646d3f8350d1632
Christian Böndgen <c.boendgen@gmail.com>
2025-07-17 11:10:21 +0200

    purge no. 1

--- apps/test/schemas.py
+++
@@ -1,8 +0,0 @@
-from typing import Literal, List, Optional
-from pydantic import BaseModel
-
-
-class SERProcessorEmotionSchema(BaseModel):
-    valence: float
-    arousal: float
-    dominance: float
