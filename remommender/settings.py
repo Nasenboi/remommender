5e99de6bfd7d7a1f817d4107505956f6d455d1f3
Christian Böndgen <c.boendgen@gmail.com>
2025-07-18 17:59:19 +0200

    add sql path env var

--- remommender/settings.py
+++ remommender/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = "remommender.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": os.getenv("SQL_DIR", BASE_DIR / "db.sqlite3"),
     }
 }
 
