93c879dbb75c70f63d2b780f210d6f14d783a30f
Christian Böndgen <c.boendgen@gmail.com>
2025-09-01 13:50:55 +0200

    create add_pre_calculated_songs

--- remommender/settings.py
+++ remommender/settings.py
@@ -27,9 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
 if not SECRET_KEY:
-    raise RuntimeError(
-        "SECRET_KEY environment variable is not set. Please provide a value for SECRET_KEY."
-    )
+    raise RuntimeError("SECRET_KEY environment variable is not set. Please provide a value for SECRET_KEY.")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
